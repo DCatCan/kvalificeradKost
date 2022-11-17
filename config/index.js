46899762dfbd38ed955a86c53e7dbc467243c5c2
Danilo Catalan Canales <63259615+DCatCan@users.noreply.github.com>
2022-11-17 01:29:23 -0300

    yes

--- config/index.js
+++
@@ -1,3 +0,0 @@
-const dev = process.env.NODE_ENV !== "production";
-
-export const server = dev ? "http://192.168.3.88:3000" : "kvalificeradKost.se";
--- data/index.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export const serialize = (el) => {
-  return JSON.parse(JSON.stringify(el));
-};
-
-export default prisma;
